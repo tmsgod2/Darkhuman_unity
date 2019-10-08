734b5d25bbdc7b2f4d6a780261fce74b0e86a559
tmsgod2 <tmsgod2@naver.com>
2019-10-08 23:18:01 +0900

    다크휴먼 자동차 겹침제거

---
+++ New Unity Project (14)/Assets/Scripts/Character.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    protected Vector2 direction;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        Move();
+    }
+    public void Move()
+    {
+            transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
---
+++ New Unity Project (14)/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        GetInput();
+        base.Update();
+    }
+
+    private void GetInput()
+    {
+        Vector2 moveVector;
+
+        moveVector.x = Input.GetAxis("Horizontal");
+        moveVector.y = Input.GetAxis("Vertical");
+
+        direction = moveVector;
+    }
+}
